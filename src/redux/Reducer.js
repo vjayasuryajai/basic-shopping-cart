2090933332699a117fcf22fba18633a20e33c8cb
jayasuryavj <35065605+vjayasuryajai@users.noreply.github.com>
2021-05-19 23:22:37 +0530

    Updated Reducer

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -11,47 +11,28 @@ const InitialState ={
  const rootreducer =(state=InitialState, action) =>{
  //console.log(action.type)
       switch(action.type){
-        
         case ADD_CART:
-      //   console.log(action.payload)
-        // console.log(state.CartItem)
-         const item = state.CartItem.filter(flterData=> action.payload.id === flterData.id)
-        // console.log(item)
-          if(!item.length)
-             { 
-            action.payload.quantity =1
-             return{
-             ...state.CartItem,
-             CartItem: [...state.CartItem,action.payload]
-                   }
+             const item = state.CartItem.filter(flterData=> action.payload.id === flterData.id)
+             if(!item.length)
+               { 
+              action.payload.quantity =1
+               return{
+                       ...state.CartItem,
+                       CartItem: [...state.CartItem,action.payload]
+                     }
                }
-
-          else {
+             else {
             action.payload.quantity +=1
             return {...state}
                }
-
-
-               case REMOVE_CART:
-                   console.log("remove")
-         // const item =
-               return{
+       case REMOVE_CART:
+              return{
                 ...state.CartItem,
                 CartItem: state.CartItem.filter(flterData=> action.payload.id !== flterData.id)
-                   
+                 }
+         default : return state
                }
-
-
-    
-        default : return state
-
-  }
-
-
-
-}
-
-
+   }
 export  const allreducer = combineReducers({
 
     root :rootreducer
